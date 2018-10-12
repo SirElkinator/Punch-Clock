b9910cacd01f8d2b22c43a76ca3c7bab3fb157fb
Benjamin Baker <bbaker4@stu.jsu.edu>
2018-10-12 12:23:25 -0500

    C

--- src/teamproject/Badge.java
+++ src/teamproject/Badge.java
@@ -10,5 +10,28 @@ package teamproject;
  * @author Chase Sparks
  */
 public class Badge {
+    
+    private String id;
+    private String description;
+    
+      public void setID(){
+        this.id = "021890C0";
+    }
+    public String getID(){
+        return id;
+    }
+    public void setDescrip(){
+        this.description = "Chapell, George R";
+    }
+    public String getDescrip(){
+        return description;
+    }
+
+    
+    public Badge(String id, String description){
+        this.id = id;
+        this.description = description;
+        
+    }
           
 }
